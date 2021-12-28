6cf350db0f6f146adcd3ff9fefdd2703361c0a9e
Marijn Stapert <mf.stapert@users.noreply.github.com>
2021-12-28 15:31:24 +0100

    small tweaks for popos

--- setup.sh
+++ setup.sh
@@ -3,31 +3,32 @@ user_home=$HOME
 path=$(realpath $0)
 dir=$(dirname $path)
 
+# add proper check
 sudo -v
-if [[ $(id -u) -ne 0 ]]
-  then echo "Please run as root"
-  exit
-fi
-
-apt_to_install=$(grep -v '^\s*$\|^\s*\#' $dir/packages/apt.txt | tr "\n" " ")
-echo "Found the following apt packages to install: $apt_to_install"
-
 sudo apt-get update
+
+apt_to_install=$(grep -v '^\s*$\|^\s*\#' $dir/packages/apt.txt)
 echo "Trying to install apt packages..."
-sudo apt-get install -y $apt_to_install
+
+while read -r pkg; do
+  echo "Installing $pkg..."
+  sudo apt-get install -y $pkg > /dev/null
+done <<< "$apt_to_install"
+
 
 apt_to_remove=$(grep -v '^\s*$\|^\s*\#' $dir/packages/apt_uninstall.txt | tr "\n" " ")
 echo "Found the following apt packages to remove: $apt_to_remove"
 
 echo "Trying to uninstall apt packages..."
-sudo apt-get remove -y $apt_to_remove
-sudo apt autoremove -y
+sudo apt-get remove -y $apt_to_remove > /dev/null
+sudo apt-get autoremove -y > /dev/null
 
 snap_to_install=$(grep -v '^\s*$\|^\s*\#' $dir/packages/snap.txt)
 echo "Trying to install snap packages..."
 
 while read -r pkg; do
-    sudo snap install $pkg
+    echo "Installing $pkg..."
+    sudo snap install $pkg > /dev/null
 done <<< "$snap_to_install"
 
 echo "Trying standalone installs..."
